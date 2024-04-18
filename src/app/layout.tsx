a49c633005268fcfe8a616af3e5edc0de7e0a444
Siluetaz <ysmaelborquezz@gmail.com>
2024-04-18 00:50:31 -0400

    providers folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import { Quicksand } from "next/font/google";
 import "./globals.css";
 import MainNav from '@/components/MainNav';
-import { ThemeProvider } from "@/components/ThemeProvider";
-import { SWRConfig } from 'swr';
 import LoadingToast from '@/components/LoadingToast';
+import Providers from '@/providers/Providers';
 
 const inter = Quicksand({
   subsets: ["latin"], weight: [
@@ -25,12 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className} >
-        <ThemeProvider attribute="class" enableSystem>
-          <MainNav />
-          <main className='container max-w-screen-2xl py-4 w-[90%]'>
-            {children}
-          </main>
-        </ThemeProvider>
+        <Providers>
+            <MainNav />
+            <main className='container max-w-screen-2xl py-4 w-[90%]'>
+              {children}
+            </main>
+        </Providers>
         <LoadingToast />
       </body>
     </html>
