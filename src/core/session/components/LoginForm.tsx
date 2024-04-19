e7f97b2e60460b8393a8d93c796edc8e6ba75b16
Benjamin Bastidas <ben.ja07@live.cl>
2024-04-19 17:38:26 -0400

    prevent double submit and loader

--- src/core/session/components/LoginForm.tsx
+++ src/core/session/components/LoginForm.tsx
@@ -1,15 +1,15 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { signIn } from "next-auth/react";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import z from "zod";
-import { useForm , SubmitHandler} from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useToast } from "@/components/ui/use-toast"
-import { CircleX, DoorClosed } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+import { CircleX, DoorClosed, LoaderCircle } from "lucide-react";
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -18,23 +18,28 @@ const formSchema = z.object({
 
 const LoginForm = () => {
   const router = useRouter();
-  const {toast} = useToast();
+  const { toast } = useToast();
+
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   const {
     handleSubmit: handleSubmitForm,
     register,
     formState: { errors },
     setError,
-  } = useForm< z.infer<typeof formSchema>>({
+  } = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   });
 
-  const onSubmit : SubmitHandler<z.infer<typeof formSchema>> = async (data) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = async (
+    data
+  ) => {
+    if (isAuthenticating) return;
+    setIsAuthenticating(true);
     try {
       const signInRes = await signIn("credentials", {
-        email: "",
-        password: "123456",
+        email: data.email,
+        password:  data.password,
         redirect: false,
       });
       if (signInRes?.ok) {
@@ -44,25 +49,37 @@ const LoginForm = () => {
           title: "Error",
           description: "Credenciales incorrectas",
           duration: 3000,
-          action: <CircleX size={24} className="text-red-400"/>,
-          
-        })
+          action: <CircleX size={24} className="text-red-400" />,
+        });
       }
     } catch (error) {
       console.error("Error al autenticar", error);
+    } finally {
+      setIsAuthenticating(false);
     }
   };
   return (
     <Card className="p-8 ">
-      <form className="space-y-4" onSubmit={handleSubmitForm(onSubmit)}>
+      <form
+        className="space-y-4"
+        onSubmit={handleSubmitForm(onSubmit)}
+      >
         <Input placeholder="Email" {...register("email")} />
         <Input
           placeholder="Password"
           type="password"
           {...register("password")}
         />
-        <Button type="submit" className="w-full">
-          Ingresar
+        <Button
+          type="submit"
+          className="w-full"
+          disabled={isAuthenticating}
+        >
+          {isAuthenticating ? (
+            <LoaderCircle size={24} className="animate-spin" />
+          ) : (
+            <span>Iniciar sesión</span>
+          )}
         </Button>
       </form>
     </Card>
